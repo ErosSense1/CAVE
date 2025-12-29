e8e246780031668019e5ce29d3268c325562f683
ErosSense1 <tiktokxsx@gmail.com>
2025-12-29 09:03:02 -0800

    shoot fix 2

--- code/js/game.js
+++ code/js/game.js
@@ -250,8 +250,8 @@ canvas.addEventListener('mousedown', (e) => {
 	tuto.style.visibility = 'hidden';
 	if (shoot_Cooldown <= 0) {
 		const myAngle = Math.atan2(
-			e.clientY - player.pos.y + player.h / 2 / 2,
-			e.clientX - player.pos.x + player.w / 2,
+			e.screenY - player.pos.y + player.h / 2 / 2,
+			e.screenX - player.pos.x + player.w / 2,
 		);
 		weapons.push(
 			new Weapon(
